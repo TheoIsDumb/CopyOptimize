859d79ff87b04935112c2028e2cf13d34b9dcbd4
theo <lesgooo1993@proton.me>
2023-07-01 01:13:09 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import inkex
+
+class CopyRaw(inkex.Effect):
+    def __init__(self):
+        inkex.Effect.__init__(self)
+
+    def template(self):
+        width = self.document.getroot().get('width')
+        height = self.document.getroot().get('height')
+        viewbox = self.document.getroot().get('viewBox')
+
+        return f'<svg width="{width}" height="{height}" version="1.1" viewBox="{viewbox}" xmlns="http://www.w3.org/2000/svg">'
+
+    def effect(self):
+        stuff = []
+        for elem in self.svg.selection:
+            stuff.append(elem.tostring().decode())
+
+        inkex.utils.debug(self.template() + ' '.join(stuff[::-1]) + "</svg>")
+
+if __name__ == '__main__':
+    CopyRaw().run()
