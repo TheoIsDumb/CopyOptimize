89bc96385d61083cb2f7a21f4f4c35ebc75b3615
theo <lesgooo1993@proton.me>
2023-07-01 16:13:28 +0530

    added scour for optimization; added scour flags; updated template to avoid scour errors

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 # coding=utf-8
 
 import inkex
+import scour
+from scour.scour import scourString, parse_args
+
+options = parse_args(['--strip-xml-prolog', '--enable-viewboxing', '--no-line-breaks', '--remove-descriptive-elements', '--enable-comment-stripping', '--enable-id-stripping', '--shorten-ids'])
 
 class CopyRaw(inkex.Effect):
     def __init__(self):
@@ -13,18 +17,19 @@ class CopyRaw(inkex.Effect):
         viewbox = self.document.getroot().get('viewBox')
         defs = self.document.getroot().defs.tostring().decode()
 
-        return f'<svg width="{width}" height="{height}" version="1.1" viewBox="{viewbox}" xmlns="http://www.w3.org/2000/svg">' + defs
-
+        return f'<svg width="{width}" height="{height}" viewBox="{viewbox}" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">' + defs
+                
     def effect(self):
         stuff = []
 
         if len(self.svg.selection) == 0:
-            inkex.errormsg(_("None selected."))
+            raise inkex.AbortExtension("None selected.")
         else:
             for elem in self.svg.selection:
                 stuff.append(elem.tostring().decode())
 
-            inkex.utils.debug(self.template() + ' '.join(stuff[::-1]) + "</svg>")
+        svg_string = self.template() + ' '.join(stuff[::-1]) + "</svg>"
+        inkex.utils.debug(scourString(svg_string, options))
 
 if __name__ == '__main__':
     CopyRaw().run()
